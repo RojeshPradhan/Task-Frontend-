9cd9edf0e9e7b1cd51d13910dd30839f23a68850
Rojesh Pradhan <Crimsonrojesh03@gmail.com>
2025-04-10 00:49:55 +0545

    updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "https://task-backend-nqrb.onrender.com";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://task-backend-nqrb.onrender.com";
+// const API_URL = "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API_URL,
